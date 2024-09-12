3417a17449717e299c06c33bcdf51fdcc86cc456
SAHAN20002 <sahan.waruna1234@gmail.com>
2024-09-12 14:41:22 +0530

    Refactor payment slip functionality and add expiration date validation

--- js/In_show.js
+++ js/In_show.js
@@ -1,11 +1,14 @@
 
-
+let date = new Date();
+let expireddate = new Date(date);
+expireddate.setMonth(expireddate.getMonth() + 1);
 
 document.getElementById("IN_01").addEventListener("click", function(){
  let paymnetLink = document.getElementById("payment_slip_01").value;
 
     if(paymnetLink == ""){
         alert('Please enter the paymnet slip link');
+        alert(expireddate);
     }else{
 
     }
