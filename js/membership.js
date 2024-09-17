988ec7c9ccd46912316445b4b695aaca2bc20312
H.D Chamika Munithunga <142832796+chamikamunithunga@users.noreply.github.com>
2024-09-17 14:11:48 +0530

    Delete js/membership.js

--- js/membership.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('studentForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    
-    alert("Student information updated.");
-});
-
-function goBack() {
-    window.history.back();
-}
